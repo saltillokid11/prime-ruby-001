9b31c7f6b85eb490db300a15e7ae36ed15820d80
Tanner Lancaster <tanner@flatironschool.com>
2018-06-05 02:42:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -20,8 +20,8 @@ def prime?(num)
   if num < 2
     return false
   else
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
+    (2..num-1).to_a.all? do |calc|
+      num % calc != 0
     end
   end
 end
