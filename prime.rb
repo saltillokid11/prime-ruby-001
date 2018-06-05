2b1011a62fdddd93b4831e2eb9b4c2236a7994a2
Tanner Lancaster <tanner@flatironschool.com>
2018-06-05 02:37:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,24 @@
-# Add  code here!
-def prime?(num)
-  squareNum = Math.sqrt(num).ceil
-  myArr = (2..num-1).to_a
-  myArr.each do |var|
-    if num < 2
-      puts "False less than 2: #{var}"
-      return false
-    else
-        return true if squareNum.all? % var == 0
-      else
-        puts "False: #{var}"
-        return false
-      end
-    end
-  end
+# # Add  code here!
+# def prime?(num)
+#   squareNum = Math.sqrt(num).ceil
+#   myArr = (2..num-1).to_a
+#   myArr.each do |var|
+#     if num < 2
+#       puts "False less than 2: #{var}"
+#       return false
+#     else
+#         return true if squareNum.all? % var == 0
+#       else
+#         puts "False: #{var}"
+#         return false
+#       end
+#     end
+#   end
+# end
+
+def is_prime?(num)
+  return if num <= 1
+  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
 end
 
 #
